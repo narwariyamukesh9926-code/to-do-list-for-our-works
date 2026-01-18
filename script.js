090d16030bbd8dd4ffb2e58d31feeefc8885de5e
narwariyamukesh9926-code <narwariyamukesh9926@gmail.com>
2026-01-18 15:20:31 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("please enter task")
+    }
+}
+
+
